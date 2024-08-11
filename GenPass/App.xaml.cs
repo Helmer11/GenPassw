0b8c452d4f83f310f564aefaded32e3bc66fccef
Helmer Salas <yonnich@gmail.com>
2024-08-11 00:00:00 -0700

    Primer commit de la aplicacion GenPass

---
+++ GenPass/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GenPass
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GenPass/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GenPass
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
